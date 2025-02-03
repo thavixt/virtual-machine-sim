fee06107703e24b930453880a4af551c66b71f60
Péter Komlósi <komlosi.peti@google.com>
2025-02-03 17:48:50 +0100

    feat: change background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,9 @@ export function Header() {
         <small>by <a target="__blank" href="https://github.com/thavixt/virtual-machine-sim">thavixt@github</a></small>
       </div>
       <div>
-        <em><small>started out to become a simplified <a target="__blank" href="https://en.wikipedia.org/wiki/Turing_machine">Turing machine</a> - turned out to be something different</small></em>
+        <em>
+          <small>started out as a simplified <a target="__blank" href="https://en.wikipedia.org/wiki/Turing_machine">Turing machine</a> - turned out to be something different</small>
+        </em>
       </div>
     </div>
   );
